7064533bbde88923ecb0452e4490714f4b9aa5b5
capodivisionetuzzi <capodivisione.tuzzi@gmail.com>
2016-11-30 22:07:37 +0000

    Added validation and create action

--- simplecodecasts_saas/app/controllers/contacts_controller.rb
+++ simplecodecasts_saas/app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "Message sent"
+            redirect_to new_contact_path notice: "Abbiamo ricevuto il tuo messaggio."
+        else
+            flash[:danger] = "Error occured"
+            redirect_to new_contact_path notice: "Si è verificato un errore."
+        end
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
--- simplecodecasts_saas/app/models/contact.rb
+++ simplecodecasts_saas/app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+  validates :name, presence: true
+  validates :email, presence: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
     
